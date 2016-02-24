e6850c252361a1d1e35491afef5cb0c294eb37ef
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2016-02-24 20:37:33 -0300

    placeholder: time parsing

--- time_parse_and_json_marshal.go
+++ time_parse_and_json_marshal.go
@@ -20,15 +20,56 @@ func (s SomeStruct) MarshalJSON() ([]byte, error) {
 	return json.Marshal(s_)
 }
 
-func main() {
+func round1() {
 	layout := "2006-01-02T15:04:05.000Z"
 	timeStr := "1816-02-23T22:19:19.019Z"
-	timeTime, _ := time.Parse(layout, timeStr)
+	timeTime, err := time.Parse(layout, timeStr)
+	if err != nil {
+		fmt.Println("Fuck! Error", err.Error())
+	}
 	s := SomeStruct{
-		AnyString:      "AnyString",
+		AnyString:      "AnyString1",
 		SomeTime:       timeTime,
 		YetAnotherBool: true,
 	}
 	content, _ := json.Marshal(s)
 	fmt.Println(string(content))
+}
+
+func round2() {
+	layout := "2006-01-02T15:04:05.000000000Z"
+	timeStr := "2016-02-03T16:34:18.430153915Z"
+	timeTime, err := time.Parse(layout, timeStr)
+	if err != nil {
+		fmt.Println("Fuck! Error", err.Error())
+	}
+	s := SomeStruct{
+		AnyString:      "AnyString2",
+		SomeTime:       timeTime,
+		YetAnotherBool: true,
+	}
+	content, _ := json.Marshal(s)
+	fmt.Println(string(content))
+}
+
+func round3() {
+	layout := "2006-01-02T15:04:05.000Z"
+	timeStr := "2011-06-12T15:38:15.678Z"
+	timeTime, err := time.Parse(layout, timeStr)
+	if err != nil {
+		fmt.Println("Fuck! Error", err.Error())
+	}
+	s := SomeStruct{
+		AnyString:      "AnyString3",
+		SomeTime:       timeTime,
+		YetAnotherBool: true,
+	}
+	content, _ := json.Marshal(s)
+	fmt.Println(string(content))
+}
+
+func main() {
+	round1()
+	round2()
+	round3()
 }
