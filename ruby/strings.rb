40aa1ee189e409bb829e5e07e0c3d0097ef531bd
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2016-02-24 21:58:23 -0300

    placeholder: strings in Ruby

---
+++ ruby/strings.rb
@@ -0,0 +1,11 @@
+sing_quoted = 'Yes, I\'m learning Ruby.'
+doub_quoted = "And...\nI'm kinda liking it"
+
+puts sing_quoted
+puts doub_quoted
+
+def multiply(a, b)
+  "#{a} times #{b} equals #{a * b}"
+end
+
+puts multiply(3, 5)
