1c98a7be7531087e21f7ccb52e434a50e0cc3a10
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2016-02-27 15:08:22 -0300

    placeholder: classes in Ruby

---
+++ ruby/classes.rb
@@ -0,0 +1,86 @@
+class Book
+
+  def initialize (title, author, pages, rating=nil)
+    @title = title
+    @author = author
+    @pages = pages
+    @rating = rating
+  end
+
+  def rating
+    @rating
+  end
+
+  def rating=(rating)
+    @rating = rating
+  end
+
+  def get_info
+    @state = 'searched'
+    "#{@title} by #{@author}, #{@pages} pages#{@rating != nil ? ", #{@rating} stars" : ''}"
+  end
+
+end
+
+book = Book.new('Blood Meridian', 'Cormac McCarthy', 337)
+p book.instance_variables
+puts book.get_info
+p book.instance_variables
+
+p book.rating
+book.rating = 4.19
+puts book.get_info
+
+
+class BetterBook
+
+  attr_accessor :title, :author, :pages, :rating
+
+  def get_info
+    @state = 'searched'
+    "#{@title} by #{@author}, #{@pages} pages#{@rating != nil ? ", #{@rating} stars" : ''}"
+  end
+
+end
+
+book = BetterBook.new
+p book.title
+book.title = 'Blood Meridian, or the Evening Redness in the West'
+puts book.title
+
+book.author = 'Cormac McCarthy'
+book.pages = 337
+book.rating = 10
+puts book.get_info
+
+
+class EvenBetterBook
+
+  attr_accessor :title, :author, :pages
+  attr_reader :rating
+
+  def initialize (title, author, pages, rating)
+    @title = title
+    @author = author
+    @pages = pages
+    self.rating = rating  # self.rating is created by attr_reader above ;-)
+  end
+
+  def rating=(rating)
+    @rating = rating unless rating > 5
+  end
+
+  def get_info
+    @state = 'searched'
+    "#{@title} by #{@author}, #{@pages} pages#{@rating != nil ? ", #{@rating} stars" : ''}"
+  end
+
+end
+
+book = EvenBetterBook.new('Blood Meridian', 'Cormac McCarthy', 337, 4.19)
+puts book.get_info
+
+book.title = 'Blood Meridian, or the Evening Redness in the West'
+book.rating = 4.99
+book.rating = 10
+puts book.get_info
