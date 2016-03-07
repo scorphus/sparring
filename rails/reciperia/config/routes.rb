0b8a211a02b2ea6dab1b64ee4ec6e2f3f23b69f5
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2016-03-06 21:15:40 -0300

    placeholder: default route in rails

--- rails/reciperia/config/routes.rb
+++ rails/reciperia/config/routes.rb
@@ -11,6 +11,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+  root 'recipes#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
