ce7f2d392f3b7aee9d87d3c917e09a751e933560
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2016-03-06 21:14:34 -0300

    placeholder: env vars and rails

--- rails/reciperia/app/models/recipe.rb
+++ rails/reciperia/app/models/recipe.rb
@@ -1,8 +1,10 @@
 class Recipe
     include HTTParty
 
-    base_uri 'http://food2fork.com/api'
-    default_params key: ENV['FOOD2FORK_KEY']
+    key_value = ENV['FOOD2FORK_KEY']
+    hostport = ENV['FOOD2FORK_SERVER_AND_PORT'] || 'food2fork.com'
+    base_uri "http://#{hostport}/api"
+    default_params key: key_value
     format :json
 
     def self.for term
