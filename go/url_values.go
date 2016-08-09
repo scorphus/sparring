f8ff63cf1763e3b992bdc1bc40527e1fb9d84b0d
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2016-08-09 20:33:25 -0300

    placeholder: url encoding and nil values in Go

--- go/url_values.go
+++ go/url_values.go
@@ -6,6 +6,9 @@ import (
 )
 
 func setDefaultParams(q url.Values) url.Values {
+	if q == nil {
+		q = url.Values{}
+	}
 	q.Set("xzz", "XZZ")
 	q.Set("foo[bar]", "1")
 	q.Set("spam[eggs]", "baz")
@@ -13,6 +16,9 @@ func setDefaultParams(q url.Values) url.Values {
 }
 
 func setSpecialParams(q url.Values) url.Values {
+	if q == nil {
+		q = url.Values{}
+	}
 	q.Set("toto", "titi")
 	q.Set("bidule[truc]", "machin")
 	return q
@@ -26,4 +32,8 @@ func main() {
 	setSpecialParams(q_)
 	fmt.Printf("q(%s):\n%s\n\n", q, q.Encode())
 	fmt.Printf("q_(%s):\n%s\n\n", q_, q_.Encode())
+	q1 := setDefaultParams(nil)
+	fmt.Printf("q1(%s):\n%s\n\n", q1, q1.Encode())
+	q2 := setSpecialParams(nil)
+	fmt.Printf("q2(%s):\n%s\n\n", q2, q2.Encode())
 }
