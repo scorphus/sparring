4e5d47a448fd7d2923a46c4e0f4c8f4d00bf1637
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2016-08-30 21:01:24 -0300

    placeholder: url encoding and indexing in Go

--- go/url_values.go
+++ go/url_values.go
@@ -27,13 +27,21 @@ func setSpecialParams(q url.Values) url.Values {
 func main() {
 	q := url.Values{}
 	q_ := setDefaultParams(q)
-	fmt.Printf("q(%s):\n%s\n\n", q, q.Encode())
-	fmt.Printf("q_(%s):\n%s\n\n", q_, q_.Encode())
+	fmt.Printf("q(%s):\n%s\n", q, q.Encode())
+	fmt.Printf("\nq_(%s):\n%s\n", q_, q_.Encode())
 	setSpecialParams(q_)
-	fmt.Printf("q(%s):\n%s\n\n", q, q.Encode())
-	fmt.Printf("q_(%s):\n%s\n\n", q_, q_.Encode())
+	fmt.Printf("\nq(%s):\n%s\n", q, q.Encode())
+	fmt.Printf("\nq_(%s):\n%s\n", q_, q_.Encode())
 	q1 := setDefaultParams(nil)
-	fmt.Printf("q1(%s):\n%s\n\n", q1, q1.Encode())
+	fmt.Printf("\nq1(%s):\n%s\n", q1, q1.Encode())
 	q2 := setSpecialParams(nil)
-	fmt.Printf("q2(%s):\n%s\n\n", q2, q2.Encode())
+	fmt.Printf("\nq2(%s):\n%s\n", q2, q2.Encode())
+	fmt.Printf("q2[toto]: %s\n", q2["toto"][0])
+	q3 := url.Values{}
+	q3, _ = url.ParseQuery("foo=var&foo=1&foo=baz")
+	fmt.Printf("\nq3(%s):\n%s\n", q3, q3.Encode())
+	foo, ok := q3["foo"]
+	if ok {
+		fmt.Printf("q3[foo]: %s\n", foo[0])
+	}
 }
