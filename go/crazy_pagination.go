6235eb9523cbf18937c6f4f4d9452ff88c1fa998
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2016-09-06 19:25:41 -0300

    placeholder: an insane pagination in Go

---
+++ go/crazy_pagination.go
@@ -0,0 +1,62 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"math"
+	"os"
+	"strconv"
+)
+
+const perPage int = 30
+
+func calcRealPage(cutRange, page int) int {
+	maxPos := cutRange + page + 2
+	return int(math.Ceil(float64(maxPos) / float64(perPage)))
+}
+
+func calcRange(cutRange, page int) (int, int) {
+	start := 0
+	end := (cutRange+page+1)%perPage + 1
+	if page > 1 {
+		start = end - 1
+	}
+	return start, end
+}
+
+func createPosts(page int) []string {
+	posts := make([]string, 0)
+	for i := 0; i < perPage; i++ {
+		n := (page-1)*perPage + i + 1
+		if n > 100 {
+			break
+		}
+		fuck := fmt.Sprintf("Posts%03d", n)
+		posts = append(posts, fuck)
+	}
+	return posts
+}
+
+func genPages(cutRange, pageStart, pageEnd int) {
+	for page := pageStart; page <= pageEnd; page++ {
+		realPage := calcRealPage(cutRange, page)
+		posts := createPosts(realPage)
+		start, end := calcRange(cutRange, page)
+		if end > len(posts) {
+			fmt.Printf("Page %02d: []\n", page)
+		} else {
+			fmt.Printf("Page %02d: %s\n", page, posts[start:end])
+		}
+	}
+}
+
+func main() {
+	args := os.Args[1:]
+	if len(args) == 0 {
+		log.Fatal("Bad arguments!")
+	}
+	cutRange, _ := strconv.Atoi(args[0])
+	pageStart, _ := strconv.Atoi(args[1])
+	pageEnd, _ := strconv.Atoi(args[2])
+	genPages(cutRange, pageStart, pageEnd)
+}
