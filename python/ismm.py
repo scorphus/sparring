d243c6f33a1a83141b0de8135516f3884ffe15df
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2016-11-07 00:39:06 -0200

    ismm: support `max` too :)

---
+++ python/ismm.py
@@ -0,0 +1,68 @@
+import sys
+
+
+class IntStackMaxMin(object):
+
+    def __init__(self):
+        self._stack = list()
+        self._max = self._min = None
+
+    def __iter__(self):
+        max_, min_ = self._max, self._min
+        for num in self._stack[::-1]:
+            if num > max_:
+                num, max_ = max_, num - max_
+            elif num < min_:
+                num, min_ = min_, min_ - num
+            yield num
+
+    def __len__(self):
+        return len(self._stack)
+
+    def __sizeof__(self):
+        extra_size = sys.getsizeof(1) * 2
+        if self._max is not None:
+            extra_size = sys.getsizeof(self._max) + sys.getsizeof(self._min)
+        return sys.getsizeof(self._stack) + extra_size
+
+    def push(self, num):
+        if not self._stack:
+            self._max = self._min = num
+        else:
+            if num < self._min:
+                num, self._min = num - self._min, num
+            elif num > self._max:
+                num, self._max = num + self._max, num
+        self._stack.append(num)
+
+    def pop(self):
+        try:
+            num = self._stack.pop()
+            if num > self._max:
+                num, self._max = self._max, num - self._max
+            elif num < self._min:
+                num, self._min = self._min, self._min - num
+            return num
+        except IndexError:
+            raise ValueError('empty sequence')
+
+    def top(self):
+        try:
+            num = self._stack[-1]
+            if num > self._max:
+                num = self._max
+            elif num < self._min:
+                num = self._min
+            return num
+        except IndexError:
+            raise ValueError('empty sequence')
+
+    def max(self):
+        if self._stack:
+            return self._max
+        raise ValueError('empty sequence')
+
+    def min(self):
+        if self._stack:
+            return self._min
+        raise ValueError('empty sequence')
--- python/ismm_test.py
+++ python/ismm_test.py
@@ -3,29 +3,29 @@ import sys
 import time
 import unittest
 
-from ism import IntStackMin
+from ismm import IntStackMaxMin
 
 
-class IntStackMinBasicTest(unittest.TestCase):
+class IntStackMaxMinBasicTest(unittest.TestCase):
 
     def test_can_create(self):
-        s = IntStackMin()
+        s = IntStackMaxMin()
         self.assertEqual(len(s), 0)
 
     def test_can_push(self):
-        s = IntStackMin()
+        s = IntStackMaxMin()
         s.push(1)
         self.assertEqual(len(s), 1)
 
     def test_can_push_more(self):
-        s = IntStackMin()
+        s = IntStackMaxMin()
         for x in range(3):
             s.push(x)
 
         self.assertEqual(len(s), 3)
 
     def test_can_pop(self):
-        s = IntStackMin()
+        s = IntStackMaxMin()
         for x in range(3):
             s.push(x)
         s.push(1)
@@ -38,7 +38,7 @@ class IntStackMinBasicTest(unittest.TestCase):
         self.assertEqual(len(s), 3)
 
     def test_can_top(self):
-        s = IntStackMin()
+        s = IntStackMaxMin()
         for x in range(3):
             s.push(x)
         s.push(1)
@@ -50,8 +50,30 @@ class IntStackMinBasicTest(unittest.TestCase):
         self.assertEqual(num, 1)
         self.assertEqual(len(s), 4)
 
+    def test_can_max(self):
+        s = IntStackMaxMin()
+        for x in [2, 3, 1, 5, 4]:
+            s.push(x)
+        self.assertEqual(len(s), 5)
+
+        num = s.max()
+
+        self.assertEqual(num, 5)
+        self.assertEqual(len(s), 5)
+
+    def test_can_max_builtin(self):
+        s = IntStackMaxMin()
+        for x in [2, 3, 1, 5, 4]:
+            s.push(x)
+        self.assertEqual(len(s), 5)
+
+        num = max(s)
+
+        self.assertEqual(num, 5)
+        self.assertEqual(len(s), 5)
+
     def test_can_min(self):
-        s = IntStackMin()
+        s = IntStackMaxMin()
         for x in [2, 3, 1, 5, 4]:
             s.push(x)
         self.assertEqual(len(s), 5)
@@ -62,7 +84,7 @@ class IntStackMinBasicTest(unittest.TestCase):
         self.assertEqual(len(s), 5)
 
     def test_can_min_builtin(self):
-        s = IntStackMin()
+        s = IntStackMaxMin()
         for x in [2, 3, 1, 5, 4]:
             s.push(x)
         self.assertEqual(len(s), 5)
@@ -73,7 +95,7 @@ class IntStackMinBasicTest(unittest.TestCase):
         self.assertEqual(len(s), 5)
 
     def test_can_iterate(self):
-        s = IntStackMin()
+        s = IntStackMaxMin()
         for x in [2, 3, 1, 5, 4]:
             s.push(x)
         self.assertEqual(len(s), 5)
@@ -82,8 +104,18 @@ class IntStackMinBasicTest(unittest.TestCase):
 
         self.assertListEqual(unstacked, [2, 3, 1, 5, 4][::-1])
 
+    def test_can_max_after_pop(self):
+        s = IntStackMaxMin()
+        for x in [2, 3, 7, 5, 4, 7]:
+            s.push(x)
+
+        s.pop()
+        num = s.max()
+
+        self.assertEqual(num, 7)
+
     def test_can_min_after_pop(self):
-        s = IntStackMin()
+        s = IntStackMaxMin()
         for x in [2, 3, 1, 5, 4, 1]:
             s.push(x)
 
@@ -92,17 +124,21 @@ class IntStackMinBasicTest(unittest.TestCase):
 
         self.assertEqual(num, 1)
 
-    def test_can_push_full_pop_empty(self):
-        s = IntStackMin()
+    def test_can_push_full_top_along_pop_empty(self):
+        s = IntStackMaxMin()
         for x in [2, 3, 1, 5, 4, 1]:
             s.push(x)
+            top = s.top()
+            self.assertEqual(top, x)
         self.assertEqual(len(s), 6)
         while len(s):
-            s.pop()
+            top = s.top()
+            pop = s.pop()
+            self.assertEqual(top, pop)
         self.assertEqual(len(s), 0)
 
 
-class IntStackMinTimeEfficiencyTest(unittest.TestCase):
+class IntStackMaxMinTimeEfficiencyTest(unittest.TestCase):
 
     range_max = 262144
     val_max = 2147483647
@@ -110,7 +146,7 @@ class IntStackMinTimeEfficiencyTest(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
         sys.stdout.write('Setting up class...\n')
-        cls.s = IntStackMin()
+        cls.s = IntStackMaxMin()
         for _ in range(cls.range_max):
             cls.s.push(int(cls.val_max * random.random()))
 
@@ -139,20 +175,29 @@ class IntStackMinTimeEfficiencyTest(unittest.TestCase):
         self.s.top()
         self.asertIsFast()
 
+    def test_max_is_fast(self):
+        self.s.max()
+        self.asertIsFast()
+
     def test_min_is_fast(self):
         self.s.min()
         self.asertIsFast()
 
-    def test_builtin_len_isnt_fast(self):
+    def test_builtin_min_isnt_fast(self):
         min(self.s)
         with self.assertRaises(AssertionError):
             self.asertIsFast()
 
+    def test_builtin_max_isnt_fast(self):
+        max(self.s)
+        with self.assertRaises(AssertionError):
+            self.asertIsFast()
 
-class IntStackMinMemoryEfficiencyTest(unittest.TestCase):
+
+class IntStackMaxMinMemoryEfficiencyTest(unittest.TestCase):
 
     def test_push(self):
-        s = IntStackMin()
+        s = IntStackMaxMin()
         aux = list()
         for _ in range(16):
             n = 1 + int(16 * random.random())
@@ -171,26 +216,32 @@ class IntStackMinMemoryEfficiencyTest(unittest.TestCase):
         self.assertEqual(increased, expected)
 
 
-class IntStackMinSafetyTest(unittest.TestCase):
+class IntStackMaxMinSafetyTest(unittest.TestCase):
 
     def test_len_an_empty_stack(self):
-        s = IntStackMin()
+        s = IntStackMaxMin()
         self.assertEqual(len(s), 0)
 
     def test_pop_from_empty_stack(self):
-        s = IntStackMin()
+        s = IntStackMaxMin()
         with self.assertRaises(ValueError) as e:
             s.pop()
         self.assertEqual(e.exception.args[0], 'empty sequence')
 
     def test_top_from_empty_stack(self):
-        s = IntStackMin()
+        s = IntStackMaxMin()
         with self.assertRaises(ValueError) as e:
             s.top()
         self.assertEqual(e.exception.args[0], 'empty sequence')
 
+    def test_max_from_empty_stack(self):
+        s = IntStackMaxMin()
+        with self.assertRaises(ValueError) as e:
+            s.max()
+        self.assertEqual(e.exception.args[0], 'empty sequence')
+
     def test_min_from_empty_stack(self):
-        s = IntStackMin()
+        s = IntStackMaxMin()
         with self.assertRaises(ValueError) as e:
             s.min()
         self.assertEqual(e.exception.args[0], 'empty sequence')
