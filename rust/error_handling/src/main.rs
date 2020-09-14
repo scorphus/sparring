112606a75180e654ec40506a993c90c51819b7f5
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2020-09-14 23:48:37 +0200

    placeholder: more error handling in Rust

--- rust/error_handling/src/main.rs
+++ rust/error_handling/src/main.rs
@@ -1,4 +1,4 @@
-use serde::{Serialize, Deserialize};
+use serde::{Deserialize, Serialize};
 
 #[derive(Serialize, Deserialize, Debug)]
 struct Project {
@@ -10,4 +10,10 @@ fn main() {
     println!("{:?}", rust);
     let servo = serde_json::from_str::<Project>(r#"{"name": "Servo",}"#);
     println!("{:?}", servo);
+
+    let xz = vec![3, 5, 9];
+    println!("Last element of xz: {:?}", xz.last());
+
+    let xz: Vec<i32> = vec![];
+    println!("Last element of xz: {:?}", xz.last());
 }
