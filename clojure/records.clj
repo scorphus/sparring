5b6f10039ab2e2cd8651a9c3dd5e259dc3f7517f
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-09-12 23:48:15 +0200

    placeholder: records in Clojure

---
+++ clojure/records.clj
@@ -0,0 +1,17 @@
+;; Define a record structure
+(defrecord Person [first-name last-name age occupation])
+
+;; Positional constructor - generated
+(def kelly (->Person "Kelly" "Keen" 32 "Programmer"))
+
+(println kelly)
+
+;; Map constructor - generated
+(def kelly (map->Person
+             {:first-name "Kelly"
+              :last-name "Keen"
+              :age 32
+              :occupation "Pro Cyclist"}))
+
+(println kelly)
+(println (:occupation kelly))
