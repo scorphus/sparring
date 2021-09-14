dad762b3765509a5b895f39cc9cc917784176b76
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-09-14 23:09:21 +0200

    placeholder: iteration in Clojure

---
+++ clojure/iteration.clj
@@ -0,0 +1,26 @@
+(dotimes [i 3]
+  (prn i))
+
+(println)
+(doseq [n (range 3)]
+  (prn n))
+(println)
+(doseq [n (range 3 6)]
+  (prn n))
+
+(println)
+(doseq [letter [:a :b :c]
+        number (range 3)]
+  (prn [letter number]))
+(println)
+(doseq [letter [:a :b :c]
+        number (range 3 6)]
+  (prn [letter number]))
+(println)
+(doseq [letter [:a :b :c]
+        number (range 5)]
+  (prn [letter number]))
+
+(println (for [letter [:a :b]
+      number (range 3)]
+  [letter number]))
