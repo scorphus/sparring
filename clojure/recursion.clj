8b18cbe8d1655531d5269f52b62b58f1875fac41
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-09-14 23:17:32 +0200

    placeholder: a bit of recursion in Clojure

---
+++ clojure/recursion.clj
@@ -0,0 +1,12 @@
+(def foo (loop [i 2]
+  (if (> (rem i 359) 0)
+    (recur (+ i 2))
+    (+ i 1))))
+(println foo)
+
+(defn increase [i]
+  (if (<= i 10)
+    (recur (inc i))
+    i))
+
+(println (increase 0))
