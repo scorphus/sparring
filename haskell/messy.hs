1644bc258915b20414966253d989f217cc40a24e
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-11-28 21:43:35 +0100

    placeholder: Functins in Haskell

--- haskell/messy.hs
+++ haskell/messy.hs
@@ -1,15 +1,26 @@
-messyMain :: IO ()
-messyMain = do
+toPart :: [Char] -> [Char]
+toPart recipient = "Dear " ++ recipient ++ ","
+
+bodyPart :: [Char] -> [Char]
+bodyPart bookTitle = "Thanks for buying " ++ bookTitle ++ "!"
+
+fromPart :: [Char] -> [Char]
+fromPart author = "Best,\n" ++ author
+
+createEmail :: [Char] -> [Char] -> [Char] -> [Char]
+createEmail recipient bookTitle author =
+  toPart recipient
+    ++ "\n\n"
+    ++ bodyPart bookTitle
+    ++ "\n\n"
+    ++ fromPart author
+
+main :: IO ()
+main = do
   print "Who is the email for?"
   recipient <- getLine
   print "What is the Title?"
   title <- getLine
   print "Who is the Author?"
   author <- getLine
-  print
-    ( "Dear " ++ recipient ++ ",\n"
-        ++ "Thanks for buying "
-        ++ title
-        ++ "\nthanks,\n"
-        ++ author
-    )
+  print (createEmail recipient title author)
