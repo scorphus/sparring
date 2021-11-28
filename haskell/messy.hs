238f82ed611e2f362e6fdf205e378319173b8e9d
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-11-28 21:25:57 +0100

    placeholder: Messy Haskell code

---
+++ haskell/messy.hs
@@ -0,0 +1,15 @@
+messyMain :: IO ()
+messyMain = do
+  print "Who is the email for?"
+  recipient <- getLine
+  print "What is the Title?"
+  title <- getLine
+  print "Who is the Author?"
+  author <- getLine
+  print
+    ( "Dear " ++ recipient ++ ",\n"
+        ++ "Thanks for buying "
+        ++ title
+        ++ "\nthanks,\n"
+        ++ author
+    )
