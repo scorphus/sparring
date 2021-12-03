aad5202caaae0868c3504b03da096c792e34c638
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-12-03 06:12:22 +0100

    placeholder: Advent of Code 2021, Day 3, Part 1 in Fish

---
+++ fish/aoc-2021-03-part-1.fish
@@ -0,0 +1,14 @@
+set gamma ""
+set lines (cat aoc-2021-03.txt)
+set bits (echo -n $lines[1] | wc -c)
+for x in (seq $bits)
+  set c (for line in $lines
+    echo $line | cut -c$x | grep 1
+  end | wc -l)
+  if test $c -ge (math (count $lines) / 2)
+    set gamma $gamma"1"
+  else
+    set gamma $gamma"0"
+  end
+end
+echo "ibase=2; $gamma * ("(string repeat -n $bits 1)" - $gamma)" | bc
