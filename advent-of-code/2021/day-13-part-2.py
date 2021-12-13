31a4a3971e76aaa9c01e9deb0275a48982b24f08
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2021-12-13 09:56:51 +0100

    placeholder: Animation of Part 2 of Day 13 of AoC 21

--- advent-of-code/2021/day-13-part-2.py
+++ advent-of-code/2021/day-13-part-2.py
@@ -1,3 +1,7 @@
+import os
+import sys
+import time
+
 with open("day-13.txt") as f:
     lines = iter(f.read().rstrip().splitlines())
 
@@ -28,19 +32,37 @@ def fold_vertically(dots, at):
     return new_dots
 
 
-def represent(dots):
-    max_x = max(x for x, y in dots)
-    max_y = max(y for x, y in dots)
+def represent(dots, crop_x=None, crop_y=None):
+    max_x = max(x for x, _ in dots)
+    max_y = max(y for _, y in dots)
+    max_x = min(max_x, crop_x or max_x)
+    max_y = min(max_y, crop_y or max_y)
     grid = [["  " for _ in range(max_x + 1)] for _ in range(max_y + 1)]
     for x, y in dots:
-        grid[y][x] = "██"
-    return "\n".join("".join(row) for row in grid)
+        if x <= max_x and y <= max_y:
+            grid[y][x] = "██"
+    return "\n".join("".join(row) for row in grid), max_x, max_y
 
 
-for along, at in folds:
+def print_grid(dots):
+    cols, lines = os.get_terminal_size()
+    grid, max_x, max_y = represent(dots, cols // 2 - 2, lines - 1)
+    print("\n" * (lines - max_y + 1) + grid + " " * (cols - 2 * max_x - 2), end="")
+
+
+animate = float(
+    next((a.split("=")[1] for a in sys.argv if a.startswith("--animate=")), 0)
+)
+
+for i, (along, at) in enumerate(folds):
     if along == "x":
         dots = fold_vertically(dots, at)
     else:
         dots = fold_horizontally(dots, at)
+    if animate:
+        if i > 0:
+            time.sleep(animate)
+        print_grid(dots)
 
-print(represent(dots))
+if not animate:
+    print(represent(dots)[0])
