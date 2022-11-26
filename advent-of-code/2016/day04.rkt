eafe974702400f3460609a9cb05aa4178625e75e
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2022-11-26 12:55:07 +0100

    placeholder: Advent of Code 2016, Day 4, Part 2 in Racket 🏓

--- advent-of-code/2016/day04.rkt
+++ advent-of-code/2016/day04.rkt
@@ -5,12 +5,20 @@
 
 (module* main #f
   (printf "part 1: ")
-  (call-with-input-file "day04.txt" (λ (port) (part-1 (in-lines port)))))
+  (call-with-input-file "day04.txt" (λ (port) (part-1 (in-lines port))))
+  (printf "part 2: ")
+  (call-with-input-file "day04.txt" (λ (port) (part-2 (in-lines port) "northpoleobjects"))))
 
 (define (part-1 lines)
   (for/sum ([l lines])
            (define room (reverse (string-split (string-replace l #rx"[][-]+" " "))))
            (if (is-real-room room) (string->number (car (cdr room))) 0)))
+
+(define (part-2 lines lookup)
+  (for/first ([l lines]
+              #:do [(define room (reverse (string-split (string-replace l #rx"[][-]+" " "))))]
+              #:when (and (is-real-room room) (room-contains room lookup)))
+    (string->number (car (cdr room)))))
 
 (define (is-real-room room)
   (define checksum (car room))
@@ -27,16 +35,40 @@
 (define (sort-letter-frequency s t)
   (if (= (cdr s) (cdr t)) (char<? (car s) (car t)) (> (cdr s) (cdr t))))
 
+(define (room-contains room lookup)
+  (define sector-id (string->number (car (cdr room))))
+  (define name (string-join (reverse (cdr (cdr room))) ""))
+  (define decrypted-name (rotate name sector-id))
+  (string-contains? decrypted-name lookup))
+
+(define (rotate string n)
+  (list->string (map (λ (c) (rotate-char c n)) (string->list string))))
+
+(define (rotate-char char n)
+  (integer->char (+ (char->integer #\a)
+                    (modulo (+ (- (char->integer char) (char->integer #\a)) (modulo n 26)) 26))))
+
 (module+ test
   (require rackunit
            rackunit/text-ui)
 
-  (define sample-input
+  (define sample-1
     (list "aaaaa-bbb-z-y-x-123[abxyz]"
           "a-b-c-d-e-f-g-h-987[abcde]"
           "not-a-real-room-404[oarel]"
           "totally-real-room-200[decoy]"))
 
-  (define suite (test-suite "part 1 tests" (test-equal? "sample input" (part-1 sample-input) 1514)))
+  (define sample-2
+    (list "aaaaa-bbb-z-y-x-123[abxyz]"
+          "a-b-c-d-e-f-g-h-987[abcde]"
+          "not-a-real-room-404[oarel]"
+          "totally-real-room-200[decoy]"
+          "qzmt-zixmtkozy-ivhz-343[zimth]"))
+
+  (define suite
+    (test-suite "day 4 tests"
+                (test-equal? "part 1 with sample input" (part-1 sample-1) 1514)
+                (test-equal? "part 2 with sample input" (part-1 sample-2) 1857)
+                (test-equal? "part 2 with sample input" (part-2 sample-2 "veryencryptedname") 343)))
 
   (run-tests suite))
