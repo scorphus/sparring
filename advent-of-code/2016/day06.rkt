c052330dbfc98aeb6d41451e524376b20c59c8cf
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2022-11-26 23:22:23 +0100

    placeholder: Advent of Code 2016, Day 6, Part 1 in Racket 🏓

---
+++ advent-of-code/2016/day06.rkt
@@ -0,0 +1,50 @@
+#lang racket/base
+
+(require racket/file
+         racket/function
+         racket/list
+         racket/string)
+
+(module* main #f
+  (let ([lines (file->lines "day06.txt")])
+    (printf "part 1: ~a~n" (part-1 lines))
+    (printf "part 2: ~a~n" (part-2 lines))))
+
+(define (part-1 lines)
+  (list->string (map (compose car (curry argmax cdr) hash->list)
+                     (for/fold ([count-maps (make-list (string-length (first lines)) #hash())]
+                                #:result count-maps)
+                               ([line lines])
+                       (map (curryr hash-update add1 0) count-maps (string->list line))))))
+
+(define (part-2 lines)
+  "part 2")
+
+(module+ test
+  (require rackunit
+           rackunit/text-ui)
+
+  (define sample
+    (list "eedadn"
+          "drvtee"
+          "eandsr"
+          "raavrd"
+          "atevrs"
+          "tsrnev"
+          "sdttsa"
+          "rasrtv"
+          "nssdts"
+          "ntnada"
+          "svetve"
+          "tesnvt"
+          "vntsnd"
+          "vrdear"
+          "dvrsen"
+          "enarar"))
+
+  (define suite
+    (test-suite "day 6 tests"
+                (test-equal? "part 1 with sample input" (part-1 sample) "easter")
+                (test-equal? "part 2 with sample input" (part-2 sample) "part 2")))
+
+  (run-tests suite))
