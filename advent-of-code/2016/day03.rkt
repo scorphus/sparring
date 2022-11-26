240bde8ab346acdd2ba848be7d81fb45d1dbb336
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2022-11-26 13:50:51 +0100

    placeholder: not repeating oneself in Racket 🏓

--- advent-of-code/2016/day03.rkt
+++ advent-of-code/2016/day03.rkt
@@ -1,33 +1,34 @@
 #lang racket/base
 
-(require racket/string)
+(require racket/file
+         racket/string)
+
+(module* main #f
+  (define lines (file->lines "day03.txt"))
+  (printf "part 1: ")
+  (part-1 lines)
+  (printf "part 2: ")
+  (part-2 lines))
+
+(define (part-1 lines)
+  (for/sum ([l lines]) (define-values (a b c) (parse-triangle l)) (one-if-triangle a b c)))
+
+(define (part-2 lines)
+  (for/fold ([sum 0] [t1 '()] [t2 '()] [t3 '()] #:result sum) ([l lines])
+    (define-values (a b c) (parse-triangle l))
+    (define t1´ (cons a t1))
+    (define t2´ (cons b t2))
+    (define t3´ (cons c t3))
+    (if (= 3 (length t1´))
+        (values
+         (+ sum (apply one-if-triangle t1´) (apply one-if-triangle t2´) (apply one-if-triangle t3´))
+         '()
+         '()
+         '())
+        (values sum t1´ t2´ t3´))))
+
+(define (parse-triangle line)
+  (apply values (map string->number (string-split line))))
 
 (define (one-if-triangle a b c)
   (if (and (< a (+ b c)) (< b (+ a c)) (< c (+ a b))) 1 0))
-
-(printf "part 1: ")
-
-(call-with-input-file
- "day03.txt"
- (λ (port)
-   (for/sum ([l (in-lines port)])
-            (define-values (a b c) (apply values (map string->number (string-split l))))
-            (one-if-triangle a b c))))
-
-(printf "part 2: ")
-
-(call-with-input-file
- "day03.txt"
- (λ (port)
-   (for/fold ([sum 0] [t1 '()] [t2 '()] [t3 '()] #:result sum) ([l (in-lines port)])
-     (define-values (a b c) (apply values (map string->number (string-split l))))
-     (define t1´ (cons a t1))
-     (define t2´ (cons b t2))
-     (define t3´ (cons c t3))
-     (if (= 3 (length t1´))
-         (values
-          (+ sum (apply one-if-triangle t1´) (apply one-if-triangle t2´) (apply one-if-triangle t3´))
-          '()
-          '()
-          '())
-         (values sum t1´ t2´ t3´)))))
