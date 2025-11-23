1a72f47d98802a38eaeebea9fc0613fd5c294892
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2025-11-23 14:41:20 +0100

    placeholder: Advent of Code 2016, Day 9 Part 2 in OCaml 🐪

--- advent-of-code/2016/day09.ml
+++ advent-of-code/2016/day09.ml
@@ -14,12 +14,29 @@ let read_file line =
         aux rest (`Repeat (data, n, String.length data) :: acc)
     | `Char c ->
         let rest = String.sub line 1 (String.length line - 1) in
-        aux rest (`Repeat (String.make 1 c, 1, 1) :: acc)
+        aux rest (`Char :: acc)
     | `End -> List.rev acc
   in
-  aux (read_line ()) []
+  aux line []
+
+let calc_length file =
+  List.fold_left
+    (fun acc item -> match item with
+     | `Repeat (_, n, len) -> acc + (n * len)
+     | `Char -> acc + 1)
+    0 file
+
+let rec calc_length_v2 file =
+  List.fold_left
+    (fun acc item -> match item with
+     | `Repeat (file, n, _) -> acc + (n * calc_length_v2 (read_file file))
+     | `Char -> acc + 1)
+    0 file
 
 let () =
-  let file = read_file () in
-  let total_length = List.fold_left (fun acc (`Repeat (_, n, len)) -> acc + (n * len)) 0 file in
-  Printf.printf "Total decompressed length: %d\n" total_length
+  let line = read_line () in
+  let file = read_file line  in
+  let total_length = calc_length file in
+  Printf.printf "Total decompressed length: %d\n" total_length;
+  let total_length = calc_length_v2 file in
+  Printf.printf "Total decompressed length v2: %d\n" total_length
