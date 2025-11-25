b956682e29bea5b83d7daca79f569e41b3236bd8
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2025-11-25 22:26:11 +0100

    placeholder: Advent of Code 2016, Day 13 Part 2 in OCaml 🐪

--- advent-of-code/2016/day13.ml
+++ advent-of-code/2016/day13.ml
@@ -11,11 +11,12 @@ let rec count_ones n = if n = 0 then 0 else (n land 1) + count_ones (n lsr 1)
 let is_open_space fave_number x y = count_ones (equation x y + fave_number) mod 2 = 0
 
 let reach fave_number target =
-  let rec aux queue visited =
+  let rec aux queue visited locations =
     match queue with
-    | [] -> -1
+    | [] -> (-1, [])
     | ((x, y), steps) :: rest ->
-        if (x, y) = target then steps
+        let locations = ((x, y), steps) :: locations in
+        if (x, y) = target then (steps, locations)
         else
           let new_steps = steps + 1 in
           let neighbors =
@@ -25,9 +26,9 @@ let reach fave_number target =
             |> List.filter (fun (coord, _) -> not (CoordSet.mem coord visited))
           in
           let new_visited = List.fold_left (fun acc (coord, _) -> CoordSet.add coord acc) visited neighbors in
-          aux (rest @ neighbors) new_visited
+          aux (rest @ neighbors) new_visited locations
   in
-  aux [ ((1, 1), 0) ] (CoordSet.singleton (1, 1))
+  aux [ ((1, 1), 0) ] (CoordSet.singleton (1, 1)) []
 
 let () =
   let fave_number, target =
@@ -35,5 +36,7 @@ let () =
     | [| _; num; x; y |] -> (int_of_string num, (int_of_string x, int_of_string y))
     | _ -> failwith "Usage: day13 <fave_number> <target_x> <target_y>"
   in
-  let steps = reach fave_number target in
-  Printf.printf "Part 1: %d\n" steps
+  let steps, locations = reach fave_number target in
+  Printf.printf "Part 1: %d\n" steps;
+  let part2 = List.fold_left (fun acc ((_, _), steps) -> if steps <= 50 then acc + 1 else acc) 0 locations in
+  Printf.printf "Part 2: %d\n" part2
