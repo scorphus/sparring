fbe3d2d125d8c3ebc4e5461ad005207c82c8e9c2
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2025-11-30 19:39:14 +0100

    placeholder: Advent of Code 2016, Day 22 Part 2 in OCaml 🐪

--- advent-of-code/2016/day22.ml
+++ advent-of-code/2016/day22.ml
@@ -50,10 +50,28 @@ let count_viable_pairs_naive cluster =
         count cluster)
     0 cluster
 
+let show_cluster cluster =
+  let max_x = List.fold_left (fun acc (x, _, _, _, _) -> max acc x) 0 cluster in
+  List.iter (fun x -> Printf.printf "    |     %2d" x) (List.init (max_x + 1) (fun x -> x));
+  List.iter
+    (fun (x, y, size, used, avail) ->
+      Printf.printf "%s%3dT/%3dT" (if x = 0 then Printf.sprintf "\n %2d | " y else " | ") size used)
+    (List.sort (fun (x1, y1, _, _, _) (x2, y2, _, _, _) -> compare (y1, x1) (y2, x2)) cluster)
+
+let part_2 cluster =
+  let steps_to_shift = 5 in
+  let max_x, max_y = List.fold_left (fun (mx, my) (x, y, _, _, _) -> (max mx x, max my y)) (0, 0) cluster in
+  let empty_x, empty_y, empty_size, _, _ = List.find (fun (_, _, _, used, _) -> used = 0) cluster in
+  let rightmost_passage_x =
+    List.fold_left (fun acc (x, _, _, used, _) -> if used > empty_size then min acc x else acc) empty_x cluster - 1
+  in
+  empty_x + empty_y - (2 * rightmost_passage_x) + max_x + ((max_x - 1) * steps_to_shift)
+
 let () =
   let cluster = read_cluster () in
   let start = Sys.time () in
   let part1 = count_viable_pairs cluster in
   Printf.printf "Part 1: %d (computed in %.6f seconds)\n" part1 (Sys.time () -. start);
   let part1_naive = count_viable_pairs_naive cluster in
-  Printf.printf "Part 1 (naive): %d (computed in %.6f seconds)\n" part1_naive (Sys.time () -. start)
+  Printf.printf "Part 1 (naive): %d (computed in %.6f seconds)\n" part1_naive (Sys.time () -. start);
+  Printf.printf "Part 2: %d\n" (part_2 cluster)
