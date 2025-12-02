190ce89588c3f9c3c094ca92c6d6ed37c711608c
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2025-12-02 13:46:29 +0100

    placeholder: Advent of Code 2025, Day 2 Part 2 in OCaml 🐪

--- advent-of-code/2025/day02.ml
+++ advent-of-code/2025/day02.ml
@@ -12,21 +12,35 @@ let read_ranges () =
   in
   aux (String.split_on_char ',' line) []
 
-let is_invalid id =
+let is_invalid_silly id =
   let id_str = string_of_int id in
   if String.length id_str mod 2 = 1 then false
   else
     let mid = String.length id_str / 2 in
     String.sub id_str 0 mid = String.sub id_str mid mid
 
-let sum_invalid_ids ranges =
+let is_invalid_sillier id =
+  let s = string_of_int id in
+  let n = String.length s in
+  let rec check_period p =
+    if p >= n then false
+    else if n mod p <> 0 then check_period (p + 1)
+    else
+      let pattern = String.sub s 0 p in
+      let rec matches i = if i >= n then true else if String.sub s i p = pattern then matches (i + p) else false in
+      if matches p then true else check_period (p + 1)
+  in
+  check_period 1
+
+let sum_invalid_ids checker ranges =
   List.fold_left
     (fun acc (a, b) ->
       acc
       + (List.init (b - a + 1) (fun i -> a + i)
-        |> List.fold_left (fun sum id -> if is_invalid id then sum + id else sum) 0))
+        |> List.fold_left (fun sum id -> if checker id then sum + id else sum) 0))
     0 ranges
 
 let () =
   let ranges = read_ranges () in
-  Printf.printf "Part 1: %d\n" (sum_invalid_ids ranges)
+  Printf.printf "Part 1: %d\n" (sum_invalid_ids is_invalid_silly ranges);
+  Printf.printf "Part 2: %d\n" (sum_invalid_ids is_invalid_sillier ranges)
