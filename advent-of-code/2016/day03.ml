15663a9160da4fdcd067860a4a0377c11cf9bfcd
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2025-12-03 21:18:24 +0100

    placeholder: Advent of Code 2016, Day 3 Part 2 in OCaml 🐪

--- advent-of-code/2016/day03.ml
+++ advent-of-code/2016/day03.ml
@@ -9,6 +9,16 @@ let is_valid_triangle (a, b, c) = a + b > c && a + c > b && b + c > a
 let count_valid_triangles triangles =
   List.fold_left (fun acc triangle -> if is_valid_triangle triangle then acc + 1 else acc) 0 triangles
 
+let count_valid_vertical triangles =
+  let valid t = if is_valid_triangle t then 1 else 0 in
+  let rec aux acc = function
+    | (a1, b1, c1) :: (a2, b2, c2) :: (a3, b3, c3) :: tail ->
+        aux (acc + valid (a1, a2, a3) + valid (b1, b2, b3) + valid (c1, c2, c3)) tail
+    | _ -> acc
+  in
+  aux 0 triangles
+
 let () =
   let triangle_sides = read_triangles () in
-  Printf.printf "Part 1: %d\n" (count_valid_triangles triangle_sides)
+  Printf.printf "Part 1: %d\n" (count_valid_triangles triangle_sides);
+  Printf.printf "Part 2: %d\n" (count_valid_vertical triangle_sides)
