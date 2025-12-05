be9fb5c2edf59a6b12387eeab9d99334bca5eaad
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2025-12-05 10:34:53 +0100

    placeholder: Advent of Code 2025, Day 5 Part 1 in OCaml 🐪

---
+++ advent-of-code/2025/day05.ml
@@ -0,0 +1,27 @@
+let parse_range line = Scanf.sscanf line "%d-%d" (fun a b -> (a, b))
+let parse_id line = Scanf.sscanf line "%d" (fun id -> id)
+
+let read parser =
+  let rec aux acc =
+    try aux ((read_line () |> parser) :: acc) with
+    | _ -> acc
+  in
+  aux []
+
+let merge_ranges ranges =
+  List.sort compare ranges
+  |> List.fold_left
+       (fun acc (a, b) ->
+         match acc with
+         | (ma, mb) :: rest when a <= mb + 1 -> (ma, max mb b) :: rest
+         | _ -> (a, b) :: acc)
+       []
+  |> List.rev
+
+let is_fresh id range = List.exists (fun (a, b) -> a <= id && id <= b) range
+
+let () =
+  let ranges = read parse_range in
+  let ids = read parse_id in
+  let merged = merge_ranges ranges in
+  Printf.printf "Part 1: %d\n" (List.filter (fun id -> is_fresh id merged) ids |> List.length)
