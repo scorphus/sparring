3674784791cc84313ab0c3b378091f7d7e4432c4
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2025-12-07 12:48:03 +0100

    placeholder: Advent of Code 2025, Day 7 Part 2 in OCaml 🐪

--- advent-of-code/2025/day07.ml
+++ advent-of-code/2025/day07.ml
@@ -1,4 +1,4 @@
-module IntSet = Set.Make (Int)
+module IntMap = Map.Make (Int)
 
 let read_lines () =
   let rec aux acc =
@@ -8,24 +8,27 @@ let read_lines () =
   aux []
 
 let find_start line = String.index line 'S'
+let add_to_map col count map = IntMap.update col (fun c -> Some (Option.value c ~default:0 + count)) map
+let sum_values map = IntMap.fold (fun _ c acc -> acc + c) map 0
 
 let split_beams line beams =
-  IntSet.fold
-    (fun i (beams, splits) ->
-      if line.[i] = '^' then (beams |> IntSet.add (i - 1) |> IntSet.add (i + 1), splits + 1)
-      else (beams |> IntSet.add i, splits))
-    beams (IntSet.empty, 0)
+  IntMap.fold
+    (fun col count (beams, splits) ->
+      if line.[col] = '^' then (beams |> add_to_map (col - 1) count |> add_to_map (col + 1) count, splits + 1)
+      else (beams |> add_to_map col count, splits))
+    beams (IntMap.empty, 0)
 
-let count_splits lines start_beam =
+let simulate lines start_beam =
   List.fold_left
     (fun (beams, splits) line ->
       let beams, splits_in_line = split_beams line beams in
       (beams, splits + splits_in_line))
-    (IntSet.empty |> IntSet.add start_beam, 0)
+    (IntMap.empty |> IntMap.add start_beam 1, 0)
     lines
 
 let () =
   let lines = read_lines () in
   let start_beam = find_start (List.hd lines) in
-  let _, splits = count_splits (List.tl lines) start_beam in
-  Printf.printf "Part 1: %d\n" splits
+  let beams, splits = simulate (List.tl lines) start_beam in
+  Printf.printf "Part 1: %d\n" splits;
+  Printf.printf "Part 2: %d\n" (sum_values beams)
