6fb72e31fcee86801174ff316f2c7f6888a85a85
Pablo Santiago Blum de Aguiar <scorphus@gmail.com>
2025-12-18 17:12:30 +0100

    placeholder: Advent of Code 2016, Day 7 Part 2 in OCaml 🐪

--- advent-of-code/2016/day07.ml
+++ advent-of-code/2016/day07.ml
@@ -16,6 +16,32 @@ let supports_tls ip =
   let supernets, hypernets = List.mapi (fun i p -> (i, p)) ip |> List.partition (fun (i, _) -> i mod 2 = 0) in
   List.exists (fun (_, s) -> is_abba s) supernets && not (List.exists (fun (_, s) -> is_abba s) hypernets)
 
+let take_abas s =
+  let rec aux i acc =
+    if i + 2 >= String.length s then acc
+    else if s.[i] <> s.[i + 1] && s.[i] = s.[i + 2] then aux (i + 1) (String.sub s i 3 :: acc)
+    else aux (i + 1) acc
+  in
+  aux 0 []
+
+let aba_to_bab aba = String.init 3 (fun i -> if i = 1 then aba.[0] else aba.[1])
+
+let contains_substring s sub =
+  let rec aux i =
+    i + String.length sub <= String.length s && (String.sub s i (String.length sub) = sub || aux (i + 1))
+  in
+  aux 0
+
+let supports_ssl ip =
+  let supernets, hypernets = List.mapi (fun i p -> (i, p)) ip |> List.partition (fun (i, _) -> i mod 2 = 0) in
+  let abas = List.concat_map (fun (_, s) -> take_abas s) supernets in
+  List.exists
+    (fun aba ->
+      let bab = aba_to_bab aba in
+      List.exists (fun (_, h) -> contains_substring h bab) hypernets)
+    abas
+
 let () =
   let ips = read_ips () in
-  Printf.printf "Part 1: %d\n" (List.filter supports_tls ips |> List.length)
+  Printf.printf "Part 1: %d\n" (List.filter supports_tls ips |> List.length);
+  Printf.printf "Part 2: %d\n" (List.filter supports_ssl ips |> List.length)
